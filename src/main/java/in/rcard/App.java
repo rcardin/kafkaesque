39d8b5a635d21855ca15376e6cbea4c8cb534658
rcardin <riccardo.cardin@gmail.com>
2020-07-22 21:52:34 +0200

    Applied Google formatting

--- src/main/java/in/rcard/App.java
+++ src/main/java/in/rcard/App.java
@@ -1,12 +1,8 @@
 package in.rcard;
 
-/**
- * Hello world!
- *
- */
+/** Hello world! */
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
