37362d04ad346aec25491472b536b526de7e0547
rcardin <riccardo.cardin@gmail.com>
2020-08-14 15:32:30 +0200

    Added the interface of KafkaesqueProducer.Builder

--- kafkaesque-core/src/main/java/in/rcard/kafkaesque/KafkaesqueProducer.java
+++ kafkaesque-core/src/main/java/in/rcard/kafkaesque/KafkaesqueProducer.java
@@ -1,7 +1,18 @@
 package in.rcard.kafkaesque;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Deserializer;
+
 public interface KafkaesqueProducer<Key, Value> {
   interface Builder<K, Key, Value> {
-  
+    Builder<K, Key, Value> toTopic(String topic);
+    Builder<K, Key, Value> withDeserializers(
+        Deserializer<Key> keyDeserializer, Deserializer<Value> valueDeserializer);
+    Builder<K, Key, Value> messages(List<ProducerRecord<Key, Value>> records);
+    Builder<K, Key, Value> waitingAtMostForEachAck(long interval, TimeUnit unit);
+    Builder<K, Key, Value> waitingForTheConsumerAtMost(long interval, TimeUnit unit);
+    KafkaesqueProducer<Key, Value> expecting();
   }
 }
