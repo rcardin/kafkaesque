47e234f7ed8093856d90ae51c17ba5422f42d5d2
rcardin <riccardo.cardin@gmail.com>
2020-08-17 15:26:46 +0200

    Started the refactoring into an interface the Kafkaesque type

--- kafkaesque-core/src/main/java/in/rcard/kafkaesque/Kafkaesque.java
+++ kafkaesque-core/src/main/java/in/rcard/kafkaesque/Kafkaesque.java
@@ -1,85 +1,74 @@
 package in.rcard.kafkaesque;
 
-import static org.reflections.ReflectionUtils.getAllMethods;
-import static org.reflections.ReflectionUtils.withName;
+import static org.reflections.ReflectionUtils.getAllConstructors;
+import static org.reflections.ReflectionUtils.withParameters;
 import static org.reflections.ReflectionUtils.withParametersCount;
-import static org.reflections.ReflectionUtils.withReturnTypeAssignableTo;
 
-import in.rcard.kafkaesque.KafkaesqueConsumer.Builder;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.reflections.Reflections;
 
-public class Kafkaesque<K> {
+public interface Kafkaesque {
 
-  private final K embeddedKafka;
-
-  private Kafkaesque(K embeddedKafka) {
-    this.embeddedKafka = embeddedKafka;
-  }
-
-  public static <K> Kafkaesque<K> newInstance(K embeddedKafka) {
-    return new Kafkaesque<>(embeddedKafka);
-  }
-
-  public <Key, Value> KafkaesqueConsumer.Builder<K, Key, Value> consume(
-      Class<Key> keyType, Class<Value> valueType) {
-    final Set<Class<? extends Builder>> buildersClass = findClassesImplementingBuilder();
-    validateBuilderClasses(buildersClass);
-    return buildersClass.stream()
-        .flatMap(this::findFactoryMethods)
+  // TODO There is a problem with the first type parameter. Try to resolve
+  <Key, Value> KafkaesqueConsumer.Builder<?, Key, Value> consume();
+  <Key, Value> KafkaesqueProducer.Builder<?, Key, Value> produce();
+  
+  static <K> Kafkaesque newInstance(K embeddedKafka) {
+    final Set<Class<? extends Kafkaesque>> kafkaesqueClasses = findClassesImplementingKafkaesque();
+    validateKafkaesqueClasses(kafkaesqueClasses);
+    return kafkaesqueClasses.stream()
+        .flatMap(clazz -> findConstructor(embeddedKafka, clazz))
         .findFirst()
-        .map((Method method) -> invokeTheFactoryMethod(method, keyType, valueType))
-        .orElseThrow(
-            () -> new AssertionError("No method found to build a new instance of a Builder"));
+        .map(ctor -> buildNewKafkaesqueInstance(embeddedKafka, ctor))
+        .orElseThrow(() -> new AssertionError("No method found to build a new instance of the Kafkaesque class"));
+  }
+  
+  static <K> Kafkaesque buildNewKafkaesqueInstance(K embeddedKafka, Constructor<?> ctor) {
+    final Object kafkaesque;
+    try {
+      kafkaesque = ctor.newInstance(embeddedKafka);
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+      throw new AssertionError("There is no possible to instantiate a new object of type Kafkaesque", e);
+    }
+    return (Kafkaesque) kafkaesque;
+  }
+  
+  
+  @SuppressWarnings("rawtypes")
+  static <K> Stream<Constructor> findConstructor(K embeddedKafka,
+      Class<? extends Kafkaesque> clazz) {
+    //noinspection unchecked
+    return getAllConstructors(clazz,
+        withParameters(embeddedKafka.getClass()),
+            withParametersCount(1)).stream();
   }
 
-  private Set<Class<? extends Builder>> findClassesImplementingBuilder() {
+  private static Set<Class<? extends Kafkaesque>> findClassesImplementingKafkaesque() {
     final Reflections reflections = new Reflections("in.rcard.kafkaesque");
-    return reflections.getSubTypesOf(Builder.class);
+    return reflections.getSubTypesOf(Kafkaesque.class);
   }
 
-  private void validateBuilderClasses(Set<Class<? extends Builder>> buildersClass) {
-    verifyIfAnyBuilderClassWasFound(buildersClass);
-    verifyIfMoreThanOneBuilderClassWasFound(buildersClass);
+  private static void validateKafkaesqueClasses(Set<Class<? extends Kafkaesque>> kafkaesqueClasses) {
+    verifyIfAnyKafkaesqueClassWasFound(kafkaesqueClasses);
+    verifyIfMoreThanOneKafkaesqueClassWasFound(kafkaesqueClasses);
   }
 
-  private void verifyIfMoreThanOneBuilderClassWasFound(
-      Set<Class<? extends Builder>> buildersClass) {
-    if (buildersClass.size() > 1) {
+  private static void verifyIfAnyKafkaesqueClassWasFound(Set<Class<? extends Kafkaesque>> kafkaesqueClasses) {
+    if (kafkaesqueClasses == null || kafkaesqueClasses.size() == 0) {
+      throw new AssertionError("No implementation of a Kafkaesque class was found");
+    }
+  }
+  
+  private static void verifyIfMoreThanOneKafkaesqueClassWasFound(
+      Set<Class<? extends Kafkaesque>> kafkaesqueClasses) {
+    if (kafkaesqueClasses.size() > 1) {
       throw new AssertionError(
           String.format(
-              "There is more than one implementation of the Kafkaesque consumer %s",
-              buildersClass.toString()));
-    }
-  }
-
-  private void verifyIfAnyBuilderClassWasFound(Set<Class<? extends Builder>> buildersClass) {
-    if (buildersClass == null || buildersClass.size() == 0) {
-      throw new AssertionError("No implementation of a Kafkaesque consumer builder found");
-    }
-  }
-
-  private Stream<Method> findFactoryMethods(Class<? extends Builder> builderClass) {
-    //noinspection unchecked
-    return getAllMethods(
-        builderClass,
-        withReturnTypeAssignableTo(Builder.class),
-        withParametersCount(1),
-        withName("newInstance"))
-        .stream();
-  }
-
-  private <Key, Value> Builder<K, Key, Value> invokeTheFactoryMethod(
-      Method method, Class<Key> keyType, Class<Value> valueType) {
-    try {
-      final Object returnedObject = method.invoke(null, embeddedKafka);
-      //noinspection unchecked
-      return (Builder<K, Key, Value>) returnedObject;
-    } catch (IllegalAccessException | InvocationTargetException e) {
-      throw new AssertionError("No static method found to build a new instance of the builder");
+              "There is more than one implementation of the Kafkaesque main class %s",
+              kafkaesqueClasses.toString()));
     }
   }
 }
