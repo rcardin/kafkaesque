19fadc6d3ac78f5cd7c5731e6706d424706681e5
rcardin <riccardo.cardin@gmail.com>
2020-09-02 21:59:08 +0200

    Added the remaining validation test on KafkaesqueProducerBuilder type

--- kafkaesque-core/src/test/java/in/rcard/kafkaesque/KafkaesqueProducerBuilderTest.java
+++ kafkaesque-core/src/test/java/in/rcard/kafkaesque/KafkaesqueProducerBuilderTest.java
@@ -5,7 +5,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import in.rcard.kafkaesque.KafkaesqueProducer.Builder;
 import in.rcard.kafkaesque.KafkaesqueProducer.KafkaesqueProducerDelegate;
 import in.rcard.kafkaesque.KafkaesqueProducer.KafkaesqueProducerDelegate.DelegateCreationInfo;
+import java.util.Collections;
 import java.util.function.Function;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,11 +18,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith({MockitoExtension.class})
 class KafkaesqueProducerBuilderTest {
-  
+
   @Mock
   private Function<DelegateCreationInfo<String, String>, KafkaesqueProducerDelegate<String, String>>
       delegateCreator;
-  
+
   private Builder<String, String> builder;
 
   @BeforeEach
@@ -40,18 +43,60 @@ class KafkaesqueProducerBuilderTest {
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessage("The topic name cannot be empty");
   }
-  
+
   @Test
   void expectingShouldThrowAnIAEIfTheTopicIsEmpty() {
     assertThatThrownBy(() -> builder.toTopic("").expecting())
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessage("The topic name cannot be empty");
   }
-  
+
   @Test
   void expectingShouldThrowAnIAEIfTheTopicIsBlank() {
     assertThatThrownBy(() -> builder.toTopic("    ").expecting())
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessage("The topic name cannot be empty");
+  }
+
+  @Test
+  void expectingShouldThrowAnIAEIfTheListOfMessagesIsNull() {
+    assertThatThrownBy(() -> builder.toTopic("topic").expecting())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("The list of records to send cannot be empty");
+  }
+
+  @Test
+  void expectingShouldThrowAnIAEIfTheListOfMessagesIsEmpty() {
+    assertThatThrownBy(() -> builder.toTopic("topic").messages(Collections.emptyList()).expecting())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("The list of records to send cannot be empty");
+  }
+
+  @Test
+  void expectingShouldThrowAnIAEIfTheKeySerializerIsNull() {
+    assertThatThrownBy(
+            () ->
+                builder
+                    .toTopic("topic")
+                    .messages(
+                        Collections.singletonList(new ProducerRecord<>("topic", "key", "message")))
+                    .withSerializers(null, new StringSerializer())
+                    .expecting())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("The serializers cannot be null");
+  }
+
+  @Test
+  void expectingShouldThrowAnIAEIfTheValueSerializerIsNull() {
+    assertThatThrownBy(
+            () ->
+                builder
+                    .toTopic("topic")
+                    .messages(
+                        Collections.singletonList(new ProducerRecord<>("topic", "key", "message")))
+                    .withSerializers(new StringSerializer(), null)
+                    .expecting())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("The serializers cannot be null");
   }
 }
