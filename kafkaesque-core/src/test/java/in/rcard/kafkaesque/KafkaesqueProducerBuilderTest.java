080d376452feae013f2c898b4ce342807f88350b
rcardin <riccardo.cardin@gmail.com>
2020-09-02 22:07:40 +0200

    Added the remaining validation test on KafkaesqueProducerBuilder type

--- kafkaesque-core/src/test/java/in/rcard/kafkaesque/KafkaesqueProducerBuilderTest.java
+++ kafkaesque-core/src/test/java/in/rcard/kafkaesque/KafkaesqueProducerBuilderTest.java
@@ -1,15 +1,16 @@
 package in.rcard.kafkaesque;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import in.rcard.kafkaesque.KafkaesqueProducer.Builder;
 import in.rcard.kafkaesque.KafkaesqueProducer.KafkaesqueProducerDelegate;
 import in.rcard.kafkaesque.KafkaesqueProducer.KafkaesqueProducerDelegate.DelegateCreationInfo;
 import java.util.Collections;
+import java.util.List;
 import java.util.function.Function;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,6 +19,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith({MockitoExtension.class})
 class KafkaesqueProducerBuilderTest {
+
+  private static final List<ProducerRecord<String, String>> MESSAGES =
+      Collections.singletonList(new ProducerRecord<>("topic", "key", "message"));
 
   @Mock
   private Function<DelegateCreationInfo<String, String>, KafkaesqueProducerDelegate<String, String>>
@@ -78,8 +82,7 @@ class KafkaesqueProducerBuilderTest {
             () ->
                 builder
                     .toTopic("topic")
-                    .messages(
-                        Collections.singletonList(new ProducerRecord<>("topic", "key", "message")))
+                    .messages(MESSAGES)
                     .withSerializers(null, new StringSerializer())
                     .expecting())
         .isInstanceOf(IllegalArgumentException.class)
@@ -92,11 +95,21 @@ class KafkaesqueProducerBuilderTest {
             () ->
                 builder
                     .toTopic("topic")
-                    .messages(
-                        Collections.singletonList(new ProducerRecord<>("topic", "key", "message")))
+                    .messages(MESSAGES)
                     .withSerializers(new StringSerializer(), null)
                     .expecting())
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessage("The serializers cannot be null");
+  }
+
+  @Test
+  void expectingShouldReturnANewInstanceOfAKafkaesqueProducer() {
+    final KafkaesqueProducer<String, String> producer =
+        builder
+            .toTopic("topic")
+            .withSerializers(new StringSerializer(), new StringSerializer())
+            .messages(MESSAGES)
+            .expecting();
+    assertThat(producer).isNotNull();
   }
 }
