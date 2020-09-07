09c0fd0ee89501eb6878339fb389287eced61d2d
rcardin <riccardo.cardin@gmail.com>
2020-09-07 22:10:03 +0200

    Added test to the Spring-Kafkaesque types

--- kafkaesque-spring-kafka/src/test/java/in/rcard/kafkaesque/SpringKafkaesqueTest.java
+++ kafkaesque-spring-kafka/src/test/java/in/rcard/kafkaesque/SpringKafkaesqueTest.java
@@ -1,64 +1,109 @@
 package in.rcard.kafkaesque;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import in.rcard.kafkaesque.SpringKafkaesqueTest.TestConfiguration;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
-import org.apache.kafka.clients.producer.ProducerConfig;
+import java.util.concurrent.TimeUnit;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 
-@SpringBootTest(classes = { TestConfiguration.class })
+@SpringBootTest(classes = {TestConfiguration.class})
 @EmbeddedKafka(
-    partitions = 1,
-    topics = { "test" }
-)
+    topics = {SpringKafkaesqueTest.CONSUMER_TEST_TOPIC, SpringKafkaesqueTest.PRODUCER_TEST_TOPIC})
 class SpringKafkaesqueTest {
-  
-  @Autowired
-  private EmbeddedKafkaBroker broker;
-  
-  @Autowired
-  private KafkaTemplate<String, String> kafkaTemplate;
-  
-  @Test
-  void consume() {
-    System.out.println("Here I am!");
-    // TODO
+
+  static final String CONSUMER_TEST_TOPIC = "test";
+  static final String PRODUCER_TEST_TOPIC = "test1";
+
+  @Autowired private EmbeddedKafkaBroker broker;
+
+  private KafkaTemplate<Integer, String> kafkaTemplate;
+  private Consumer<Integer, String> consumer;
+
+  @BeforeEach
+  void setUp() {
+    setUpProducer();
+    setUpConsumer();
   }
-  
-  @Test
-  void produce() {
-    // TODO
+
+  private void setUpProducer() {
+    final DefaultKafkaProducerFactory<Integer, String> producerFactory =
+        new DefaultKafkaProducerFactory<>(KafkaTestUtils.producerProps(broker));
+    kafkaTemplate = new KafkaTemplate<>(producerFactory);
+    kafkaTemplate.setDefaultTopic(CONSUMER_TEST_TOPIC);
   }
-  
+
+  private void setUpConsumer() {
+    Map<String, Object> consumerProps =
+        KafkaTestUtils.consumerProps(PRODUCER_TEST_TOPIC, "false", broker);
+    DefaultKafkaConsumerFactory<Integer, String> cf =
+        new DefaultKafkaConsumerFactory<>(consumerProps);
+    consumer = cf.createConsumer();
+    broker.consumeFromAllEmbeddedTopics(consumer);
+  }
+
+  @AfterEach
+  void tearDown() {
+    consumer.close();
+  }
+
+  @Test
+  void consumeShouldConsumeMessagesProducesFromOutsideProducer() {
+    kafkaTemplate.sendDefault(1, "data1");
+    kafkaTemplate.sendDefault(2, "data2");
+    new SpringKafkaesque(broker)
+        .<Integer, String>consume()
+        .fromTopic(CONSUMER_TEST_TOPIC)
+        .waitingAtMost(1L, TimeUnit.SECONDS)
+        .waitingEmptyPolls(5, 100L, TimeUnit.MILLISECONDS)
+        .withDeserializers(new IntegerDeserializer(), new StringDeserializer())
+        .expecting()
+        .poll()
+        .havingRecordsSize(2)
+        .assertingThatPayloads(Matchers.containsInAnyOrder("data1", "data2"));
+  }
+
+  @Test
+  void produceShouldProduceMessageForOutsideConsumer() {
+    new SpringKafkaesque(broker)
+        .<Integer, String>produce()
+        .messages(
+            Arrays.asList(
+                new ProducerRecord<>(PRODUCER_TEST_TOPIC, 1, "value1"),
+                new ProducerRecord<>(PRODUCER_TEST_TOPIC, 2, "value2")))
+        .toTopic("test1")
+        .withSerializers(new IntegerSerializer(), new StringSerializer())
+        .expecting()
+        .assertingAfterEach(
+            record -> {
+              final ConsumerRecord<Integer, String> consumerRecord =
+                  KafkaTestUtils.getSingleRecord(consumer, PRODUCER_TEST_TOPIC);
+              assertThat(consumerRecord)
+                  .hasFieldOrPropertyWithValue("key", record.key())
+                  .hasFieldOrPropertyWithValue("value", record.value());
+            });
+  }
+
   @Configuration
-  static class TestConfiguration {
-    @Bean
-    ProducerFactory<String, String> producerFactory() {
-      return new DefaultKafkaProducerFactory<>(producerConfigs());
-    }
-  
-    @Bean
-    Map<String, Object> producerConfigs() {
-      Map<String, Object> props = new HashMap<>();
-      props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-      props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-      props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-      return props;
-    }
-  
-    @Bean
-    KafkaTemplate<String, String> kafkaTemplate() {
-      return new KafkaTemplate<>(producerFactory());
-    }
-  }
+  static class TestConfiguration {}
 }
