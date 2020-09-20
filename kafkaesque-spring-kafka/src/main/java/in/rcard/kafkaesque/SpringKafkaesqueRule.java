dd4c34371c7f558dde4812b45033fc0d3eef5c94
rcardin <riccardo.cardin@gmail.com>
2020-09-20 15:06:25 +0200

    [KFKESQ-5] Added a JUnit4 Rule to automatically instantiate a SpringKafkaesque object

---
+++ kafkaesque-spring-kafka/src/main/java/in/rcard/kafkaesque/SpringKafkaesqueRule.java
@@ -0,0 +1,42 @@
+package in.rcard.kafkaesque;
+
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+
+/**
+ * A wrapper Junit4 Rule to {@link EmbeddedKafkaRule}. It creates an instance of the
+ * {@link SpringKafkaesque} class. It is recommended to use the rule as a {@code @ClassRule}.
+ */
+public class SpringKafkaesqueRule extends EmbeddedKafkaRule {
+  
+  private SpringKafkaesque kafkaesque;
+  
+  public SpringKafkaesqueRule(int count) {
+    super(count);
+    this.kafkaesque = new SpringKafkaesque(getEmbeddedKafka());
+  }
+  
+  public SpringKafkaesqueRule(int count, boolean controlledShutdown, String... topics) {
+    super(count, controlledShutdown, topics);
+    this.kafkaesque = new SpringKafkaesque(getEmbeddedKafka());
+  }
+  
+  public SpringKafkaesqueRule(int count, boolean controlledShutdown, int partitions,
+      String... topics) {
+    super(count, controlledShutdown, partitions, topics);
+    this.kafkaesque = new SpringKafkaesque(getEmbeddedKafka());
+  }
+  
+  @Override
+  public void before() {
+    super.before();
+  }
+  
+  @Override
+  public void after() {
+    super.after();
+  }
+  
+  public Kafkaesque getKafkaesque() {
+    return kafkaesque;
+  }
+}
