e94233c84b0d5a0980e50bc438fb0c35f1485162
rcardin <riccardo.cardin@gmail.com>
2020-09-20 15:23:32 +0200

    [KFKESQ-5] Fix the documentation

--- kafkaesque-spring-kafka/src/main/java/in/rcard/kafkaesque/SpringKafkaesqueRule.java
+++ kafkaesque-spring-kafka/src/main/java/in/rcard/kafkaesque/SpringKafkaesqueRule.java
@@ -3,23 +3,32 @@ package in.rcard.kafkaesque;
 import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
 
 /**
- * A wrapper Junit4 Rule to {@link EmbeddedKafkaRule}. It creates an instance of the
+ * A Junit4 Rule wrapping the {@link EmbeddedKafkaRule}. It creates an instance of the
  * {@link SpringKafkaesque} class. It is recommended to use the rule as a {@code @ClassRule}.
  */
 public class SpringKafkaesqueRule extends EmbeddedKafkaRule {
   
   private final SpringKafkaesque kafkaesque;
   
+  /**
+   * @see EmbeddedKafkaRule#EmbeddedKafkaRule(int)
+   */
   public SpringKafkaesqueRule(int count) {
     super(count);
     this.kafkaesque = new SpringKafkaesque(getEmbeddedKafka());
   }
   
+  /**
+   * @see EmbeddedKafkaRule#EmbeddedKafkaRule(int, boolean, String...)
+   */
   public SpringKafkaesqueRule(int count, boolean controlledShutdown, String... topics) {
     super(count, controlledShutdown, topics);
     this.kafkaesque = new SpringKafkaesque(getEmbeddedKafka());
   }
   
+  /**
+   * @see EmbeddedKafkaRule#EmbeddedKafkaRule(int, boolean, int, String...)
+   */
   public SpringKafkaesqueRule(int count, boolean controlledShutdown, int partitions,
       String... topics) {
     super(count, controlledShutdown, partitions, topics);
@@ -36,6 +45,10 @@ public class SpringKafkaesqueRule extends EmbeddedKafkaRule {
     super.after();
   }
   
+  /**
+   * Returns the {@link SpringKafkaesque} instance.
+   * @return A SpringKafkaesque instance
+   */
   public Kafkaesque getKafkaesque() {
     return kafkaesque;
   }
