eea95bdbce99bbaad861aa9ddd8fa1ba21372739
rcardin <riccardo.cardin@gmail.com>
2020-11-11 22:03:03 +0100

    Added the test to the methods that create the input and the output topic

--- kafkaesque-spring-kafka/src/test/java/in/rcard/kafkaesque/SpringKafkaesqueTest.java
+++ kafkaesque-spring-kafka/src/test/java/in/rcard/kafkaesque/SpringKafkaesqueTest.java
@@ -2,9 +2,10 @@ package in.rcard.kafkaesque;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import in.rcard.kafkaesque.KafkaesqueOutputTopic.Message;
 import in.rcard.kafkaesque.KafkaesqueProducer.Record;
-import in.rcard.kafkaesque.SpringKafkaesqueTest.TestConfiguration;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.apache.kafka.clients.consumer.Consumer;
@@ -19,7 +20,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -27,18 +27,21 @@ import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 
-@SpringBootTest(classes = {TestConfiguration.class})
+@SpringBootTest(classes = {SpringKafkaesque.class})
 @EmbeddedKafka(
     topics = {
-        SpringKafkaesqueTest.CONSUMER_TEST_TOPIC,
-        SpringKafkaesqueTest.PRODUCER_TEST_TOPIC
-    }
-)
+      SpringKafkaesqueTest.CONSUMER_TEST_TOPIC,
+      SpringKafkaesqueTest.CONSUMER_TEST_TOPIC_1,
+      SpringKafkaesqueTest.PRODUCER_TEST_TOPIC,
+      SpringKafkaesqueTest.PRODUCER_TEST_TOPIC_1
+    })
 class SpringKafkaesqueTest {
 
   static final String CONSUMER_TEST_TOPIC = "test";
   static final String PRODUCER_TEST_TOPIC = "test1";
-
+  static final String CONSUMER_TEST_TOPIC_1 = "test2";
+  static final String PRODUCER_TEST_TOPIC_1 = "test3";
+  
   @Autowired private EmbeddedKafkaBroker broker;
 
   private KafkaTemplate<Integer, String> kafkaTemplate;
@@ -54,7 +57,6 @@ class SpringKafkaesqueTest {
     final DefaultKafkaProducerFactory<Integer, String> producerFactory =
         new DefaultKafkaProducerFactory<>(KafkaTestUtils.producerProps(broker));
     kafkaTemplate = new KafkaTemplate<>(producerFactory);
-    kafkaTemplate.setDefaultTopic(CONSUMER_TEST_TOPIC);
   }
 
   private void setUpConsumer() {
@@ -73,8 +75,8 @@ class SpringKafkaesqueTest {
 
   @Test
   void consumeShouldConsumeMessagesProducesFromOutsideProducer() {
-    kafkaTemplate.sendDefault(1, "data1");
-    kafkaTemplate.sendDefault(2, "data2");
+    kafkaTemplate.send(CONSUMER_TEST_TOPIC, 1, "data1");
+    kafkaTemplate.send(CONSUMER_TEST_TOPIC, 2, "data2");
     new SpringKafkaesque(broker)
         .<Integer, String>consume()
         .fromTopic(CONSUMER_TEST_TOPIC)
@@ -105,6 +107,36 @@ class SpringKafkaesqueTest {
             });
   }
 
-  @Configuration
-  static class TestConfiguration {}
+  @Test
+  void createInputTopicShouldCreateAStructureThatSendToTheBrokerTheRecords() {
+    final KafkaesqueInputTopic<Integer, String> inputTopic =
+        new SpringKafkaesque(broker)
+            .createInputTopic(PRODUCER_TEST_TOPIC_1, new IntegerSerializer(), new StringSerializer());
+    inputTopic.pipeRecordList(
+        Arrays.asList(Record.of(100, "One hundred"), Record.of(200, "Two hundred")));
+    new SpringKafkaesque(broker)
+        .<Integer, String>consume()
+        .fromTopic(PRODUCER_TEST_TOPIC_1)
+        .waitingAtMost(1L, TimeUnit.SECONDS)
+        .waitingEmptyPolls(5, 100L, TimeUnit.MILLISECONDS)
+        .withDeserializers(new IntegerDeserializer(), new StringDeserializer())
+        .expecting()
+        .havingRecordsSize(2)
+        .assertingThatPayloads(Matchers.containsInAnyOrder("One hundred", "Two hundred"))
+        .andCloseConsumer();
+  }
+
+  @Test
+  void createOutputTopicShouldCreateAStructureTheReadsFromTheBrokerTheMessages() {
+    kafkaTemplate.send(CONSUMER_TEST_TOPIC_1, 300, "Three hundred");
+    kafkaTemplate.send(CONSUMER_TEST_TOPIC_1, 400, "Four hundred");
+    final KafkaesqueOutputTopic<Integer, String> outputTopic =
+        new SpringKafkaesque(broker)
+            .createOutputTopic(
+                CONSUMER_TEST_TOPIC_1, new IntegerDeserializer(), new StringDeserializer());
+    final List<Message<Integer, String>> messages = outputTopic.readRecordsToList();
+    assertThat(messages)
+        .extracting("value")
+        .containsExactlyInAnyOrder("Three hundred", "Four hundred");
+  }
 }
