28608d394ca8301d96e5ca479c42659463339077
rcardin <riccardo.cardin@gmail.com>
2021-10-20 22:11:36 +0200

    [KFKESQ-9] Added the first implementation for the consumer in module live

--- kafkaesque-live-kafka/src/main/java/in/rcard/kafkaesque/LiveKafkaesque.java
+++ kafkaesque-live-kafka/src/main/java/in/rcard/kafkaesque/LiveKafkaesque.java
@@ -4,53 +4,90 @@ import in.rcard.kafkaesque.KafkaesqueConsumer.Builder;
 import in.rcard.kafkaesque.KafkaesqueConsumer.KafkaesqueConsumerDelegate;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.RecordMetadata;
 
 public class LiveKafkaesque implements Kafkaesque {
-  
+
   private final String brokersUrl;
-  
+
   public LiveKafkaesque(String brokersUrl) {
     this.brokersUrl = brokersUrl;
   }
-  
+
   @Override
   public <Key, Value> Builder<Key, Value> consume() {
     return Builder.newInstance(
         creationInfo -> {
-          
           try {
             final Properties props = new Properties();
-            props.put("client.id", InetAddress.getLocalHost().getHostName());
-            props.put("group.id", "kafkaesque-consumer");
-            props.put("bootstrap.servers", brokersUrl);
+            props.put(ConsumerConfig.CLIENT_ID_CONFIG, InetAddress.getLocalHost().getHostName());
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, "kafkaesque-consumer");
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokersUrl);
+            props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+            props.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                creationInfo.getKeyDeserializer().getClass());
+            props.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                creationInfo.getValueDeserializer().getClass());
             final KafkaConsumer<Key, Value> kafkaConsumer = new KafkaConsumer<>(props);
             kafkaConsumer.subscribe(List.of(creationInfo.getTopic()));
-  
-            return new KafkaesqueConsumerDelegate<Key, Value>() {
+
+            return new KafkaesqueConsumerDelegate<>() {
               @Override
               public List<ConsumerRecord<Key, Value>> poll() {
-                return null;
+                final ConsumerRecords<Key, Value> polled =
+                    kafkaConsumer.poll(Duration.ofMinutes(1L));
+                final List<ConsumerRecord<Key, Value>> records = new ArrayList<>();
+                polled.records(creationInfo.getTopic()).forEach(records::add);
+                return records;
               }
-    
+
               @Override
               public void close() {
-                // TODO Use a countdown latch?
                 kafkaConsumer.close();
               }
             };
-            
+
           } catch (UnknownHostException e) {
-            // TODO
+            throw new IllegalStateException(e);
           }
         });
   }
-  
+
   @Override
   public <Key, Value> KafkaesqueProducer.Builder<Key, Value> produce() {
-    return null;
+    return new KafkaesqueProducer.Builder<>(
+        creationInfo -> {
+          final Properties props = new Properties();
+          props.put(ProducerConfig.ACKS_CONFIG, "all");
+          props.put(
+              ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+              creationInfo.getKeySerializer().getClass());
+          props.put(
+              ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+              creationInfo.getValueSerializer().getClass());
+          Producer<Key, Value> producer = new KafkaProducer<>(props);
+          return record -> {
+            producer.send(record, new Callback() {
+              @Override
+              public void onCompletion(RecordMetadata metadata, Exception exception) {
+                // TODO
+              }
+            })
+          }
+        });
   }
 }
